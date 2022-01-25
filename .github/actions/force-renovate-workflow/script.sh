8def7465aeca4758baaa45d3961b54ec2fb1a22d
alexserov@gmail.com <jexaser@gmail.com>
2022-01-25 14:38:26 +0300

    fix

--- .github/actions/force-renovate-workflow/script.sh
+++ .github/actions/force-renovate-workflow/script.sh
@@ -4,7 +4,7 @@ paginate () { # "method" "URL" "items getter"
     while true
     do
         ((page++))
-        local value=$(echo $(curl -s -X $1 $2 -H "Accept: application/vnd.github.v3+json" -G -H "Authorization: Bearer ${{github.token}}" -d per_page=100 -d page=$page) | jq $3)
+        local value=$(echo $(curl -s -X $1 $2 -H "Accept: application/vnd.github.v3+json" -G -H "Authorization: Bearer $GITHUB_TOKEN" -d per_page=100 -d page=$page) | jq $3)
         if [ $(echo $value | jq -r ". | length") == 0 ]; then
             echo $result
             return 0
@@ -17,17 +17,17 @@ paginate () { # "method" "URL" "items getter"
 }
 
 
-all_requests=$(paginate GET "https://api.github.com/repos/${{github.repository}}/pulls?state=open")
+all_requests=$(paginate GET "https://api.github.com/repos/$1/pulls?state=open")
 renovate_request_refs=$(echo $all_requests | jq 'map(select(.user.login | contains("renovate"))) | .[] | .head.ref')
 
 for request_ref in $(echo $renovate_request_refs | jq -r .); do
-    all_checks=$(paginate GET "https://api.github.com/repos/${{github.repository}}/commits/$request_ref/check-runs" .check_runs)
+    all_checks=$(paginate GET "https://api.github.com/repos/$1/commits/$request_ref/check-runs" .check_runs)
     failed_checks=$(echo $all_checks | jq 'map(select((.conclusion != null) and (.conclusion | contains("failure"))))')
 
     for check_id in $(echo $failed_checks | jq -r .[].check_suite.id); do
         curl -X POST \
             -H "Accept: application/vnd.github.v3+json" \
-            -H "Authorization: Bearer ${{github.token}}" \
-            "https://api.github.com/repos/${{github.repository}}/check-suites/${check_id}/rerequest"
+            -H "Authorization: Bearer $GITHUB_TOKEN" \
+            "https://api.github.com/repos/$1/check-suites/${check_id}/rerequest"
     done
 done
